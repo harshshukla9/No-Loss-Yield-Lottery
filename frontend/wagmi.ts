7e15d985417b04070da01e030f453a25568e438c
leetebbs <leetebbs@ymail.com>
2025-06-20 09:37:51 +0100

    add project id wor rainbow kit

--- frontend/wagmi.ts
+++ frontend/wagmi.ts
@@ -1,30 +1,19 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { arbitrum, base, mainnet, optimism, polygon, sepolia, anvil } from "wagmi/chains";
 
-const sepoliaRpcUrl = process.env.NEXT_PUBLIC_RPC_URL;
-if (!sepoliaRpcUrl) {
-  throw new Error("NEXT_PUBLIC_RPC_URL is not set in your environment variables.");
-}
-const customSepolia = {
-  ...sepolia,
-  rpcUrls: {
-    default: { http: [sepoliaRpcUrl] },
-    public: { http: [sepoliaRpcUrl] },
-  },
-};
+
 
 export const config = getDefaultConfig({
   appName: "RainbowKit Demo",
-  projectId: "YOUR_PROJECT_ID", // Keep or remove if unnecessary
+  projectId: process.env.NEXT_PUBLIC_PROJECTID || "",
   chains: [
-    customSepolia,
     mainnet,
     polygon,
     optimism,
     arbitrum,
     base,
     anvil, // ✅ Add localhost for Hardhat
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [customSepolia] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
   ssr: true,
 });
